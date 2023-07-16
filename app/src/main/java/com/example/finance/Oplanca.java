78f29bca0adf7567334edab98a44546b03e0536c
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-16 03:33:51 -0300

    mudanças em alguns layouts e classes

--- app/src/main/java/com/example/finance/Oplanca.java
+++ app/src/main/java/com/example/finance/Oplanca.java
@@ -2,6 +2,7 @@ package com.example.finance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
