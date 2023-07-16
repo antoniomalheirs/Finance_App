52b3e7ec8b0e9ceec12ade0fb3b8d9201e9354c9
antoniomalheirs <antoniomalhe@gmail.com>
2023-07-16 01:50:56 -0300

    Ajustes na logica de funcionamento da tela de historico

--- app/src/main/java/com/example/finance/Ophistorico.java
+++ app/src/main/java/com/example/finance/Ophistorico.java
@@ -4,15 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class Ophistorico extends AppCompatActivity {
+public class Ophistorico extends AppCompatActivity
+{
+    private final Handler handler = new Handler();
+    private Runnable runnable;
+
+    ListView displaylistdados;
+    EditText descricao,data;
+    TextView displayuser;
+
+    DespreceBanco BancoD;
+    int iduser;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ophistorico);
+
+        displayuser = findViewById(R.id.displayuser);
+        displaylistdados = findViewById(R.id.displaylistdados);
+        data = findViewById(R.id.displaydata);
+        descricao = findViewById(R.id.displaydescricao);
+
+        BancoD = DespreceBanco.getInstance(this);
+        iduser = getIntent().getIntExtra ("idu",0);
+        BancoD.exibirDadosNoListView(displaylistdados, getBaseContext(),iduser);
+
+        String name = getIntent().getStringExtra("nome");
+        displayuser.setText(name + " este é seu");
     }
 
     public void onClick(View v)
